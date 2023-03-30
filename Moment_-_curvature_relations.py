3c23cab712630015baca1d8a77f99f9ea9874a91
joaopxf <129347489+joaopxf@users.noreply.github.com>
2023-03-29 22:47:03 -0300

    Update Moment_-_curvature_relations.py

--- Moment_-_curvature_relations.py
+++ Moment_-_curvature_relations.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 # PROGRAM TO DERIVE MOMENT-CURVATURE RELATIONS FOR RC MEMBERS
 
